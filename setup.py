cbe316ddd9272a2111202a303e8370a6fadf1622
elenaacinapura <elena.acinapura@gmail.com>
2020-03-26 10:27:36 +0100

    distribution setup

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='lib-physics',
+      version='1.0',
+      description='Physics data analysis routines',
+      author='Elena Acinapura',
+      author_email='elena.acinapura@gmail.com',
+      py_modules=['libphysics'],
+     )
