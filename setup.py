802b33836b3fb252ebdafd34e56f18c8f2784967
elenaacinapura <elena.acinapura@gmail.com>
2020-07-03 11:12:50 +0200

    setup

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from distutils.core import setup
 
 setup(name='lib-physics',
-      version='1.0.3',
+      version='1.0.4',
       description='Physics data analysis routines',
       author='Elena Acinapura',
       author_email='elena.acinapura@gmail.com',
